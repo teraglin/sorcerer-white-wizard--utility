baba04e70e176fe72b33c8adf47db87915e69723
Aidan Kirvan <apkirvan@hotmail.com>
2022-01-10 14:51:09 +1100

    Task: initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import MainPage from './pages/index';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <MainPage />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
